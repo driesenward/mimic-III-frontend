f292c9e442fb985ff4ca105873c512bc7336dddb
Ward Driesen <ward.driesen@student.kdg.be>
2021-08-10 17:00:40 +0200

    Quasar setup

---
+++ src/components/VisualizationComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Visualization</h1>
+</template>
+
+<script>
+export default {
+  name: "VisualizationComponent",
+};
+</script>
+
+<style></style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page class="row items-center justify-evenly"> </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "PageIndex",
+  setup() {},
+});
+</script>
